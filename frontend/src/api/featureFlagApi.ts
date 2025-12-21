8b5e9b3996c3dc7885d52a548debf5b76ce5e6c5
Aditya Tile <adityatile99@gmail.com>
2025-12-21 12:20:57 +0530

    feat: setup docker compose with frontend, backend, and postgres

--- frontend/src/api/featureFlagApi.ts
+++ frontend/src/api/featureFlagApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import type { FeatureFlag } from "../types/FeatureFlag";
 
-export const API_BASE_URL = "http://localhost:8080/api/v1/flags";
+export const API_BASE_URL = "http://backend:8080/api/v1/flags";
 
 export const getAllFlags = async (): Promise<FeatureFlag[]> => {
   const response = await axios.get(API_BASE_URL);
